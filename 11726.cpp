8008ae087cabecf1dd9dd78a51b17a85a58c1983
andrew <statco19@gmail.com>
2022-10-10 20:25:00 +0900

    dp s3

---
+++ 11726.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pb push_back
+#define ioa insert_or_assign
+#define umap unordered_map
+#define prq priority_queue
+using namespace std;
+typedef pair<int,int> pii;
+typedef vector<int> vint;
+const int INF = 0x3f3f3f3f; const int mINF = 0xc0c0c0c0;
+const ll LINF = 0x3f3f3f3f3f3f3f3f; const ll mLINF = 0xc0c0c0c0c0c0c0c0;
+
+int n;
+int dp[1001];
+const int mod = 10007;
+
+void sol() {
+	dp[1] = 1;
+	dp[2] = 2;
+	for(int i=3;i<=n;++i) {
+		dp[i] = dp[i-1] % mod + dp[i-2] % mod;
+	}
+	cout << dp[n] % mod << '\n';
+
+	return;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	cin >> n;
+	sol();
+
+	return 0;
+}
