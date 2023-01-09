b4df4ce8033d2bb4201b1087a62ce05f980dbf93
andrew <statco19@gmail.com>
2023-01-09 22:24:27 +0900

    dfs+dp g2 *

---
+++ 1103.cpp
@@ -0,0 +1,144 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define vt vector
+#define ll long long
+#define pb push_back
+#define pf push_front
+#define all(c) (c).begin(), (c).end()
+#define rev(c) (c).rbegin(), (c).rend()
+#define sz(x) (int)(x).size()
+#define mset(ar, val) memset(ar, val, sizeof(ar))
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vll vector<long long>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define en '\n'
+#define ioa insert_or_assign
+#define umap unordered_map
+#define prq priority_queue
+#define F first
+#define S second
+
+#define F_OR(i, a, b, s) for (int i=(a); (s)>0?i<(b):i>(b); i+=(s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
+#define GET5(a, b, c, d, e, ...) e
+#define F_ORC(...) GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1)
+#define FOR(...) F_ORC(__VA_ARGS__)(__VA_ARGS__)
+
+#define E_ACH2(x, a) for (auto &x : a)
+#define E_ACH3(x, y, a) for (auto &[x, y] : a)
+#define E_ACH4(x, y, z, a) for (auto &[x, y, z] : a)
+#define E_ACHC(...) GET5(__VA_ARGS__, E_ACH4, E_ACH3, E_ACH2)
+#define EACH(...) E_ACHC(__VA_ARGS__)(__VA_ARGS__)
+
+string to_string(char c) {
+    return string(1, c);
+}
+string to_string(bool b) {
+    return b?"true":"false";
+}
+string to_string(const char* s) {
+    return string(s);
+}
+string to_string(string s) {
+    return s;
+}
+string to_string(vt<bool> v) {
+    string res;
+    FOR(sz(v))
+        res+=char('0'+v[i]);
+    return res;
+}
+
+vector<string> split (const string &s, char delim) {
+    vector<string> result;
+    stringstream ss (s);
+    string item;
+
+    while (getline (ss, item, delim)) {
+        result.push_back (item);
+    }
+
+    return result;
+}
+
+vector<string> split (const string &s, string delimiter) {
+    size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+    string token;
+    vector<string> res;
+
+    while ((pos_end = s.find (delimiter, pos_start)) != string::npos) {
+        token = s.substr (pos_start, pos_end - pos_start);
+        pos_start = pos_end + delim_len;
+        res.push_back (token);
+    }
+
+    res.push_back (s.substr (pos_start));
+    return res;
+}
+
+const int d4r[4] = {-1,0,1,0}; const int d4c[4] = {0,1,0,-1};
+const int d8r[8] = {-1,-1,0,1,1,1,0,-1}; const int d8c[8] = {0,1,1,1,0,-1,-1,-1};
+const int INF = 0x3f3f3f3f; const int mINF = 0xc0c0c0c0;
+const ll LINF = 0x3f3f3f3f3f3f3f3f; const ll mLINF = 0xc0c0c0c0c0c0c0c0;
+int T = 1;
+
+int N, M;
+int bd[51][51];
+int dp[51][51];
+bool vis[51][51];
+int ans;
+
+int dfs(int r, int c) {
+    if (r<1 || r>N || c<1 || c>M || bd[r][c] == -1) return 0;
+
+    if (vis[r][c]) {
+        cout << -1 << en;
+        exit(0);
+    }
+
+    if (dp[r][c] != -1) return dp[r][c];
+
+    vis[r][c] = 1;
+    dp[r][c] = 0;
+    for (int d = 0; d < 4; ++d) {
+        int nr = r + d4r[d] * bd[r][c];
+        int nc = c + d4c[d] * bd[r][c];
+
+        dp[r][c] = max(dp[r][c], dfs(nr, nc) + 1);
+    }
+    vis[r][c] = 0;
+    return dp[r][c];
+}
+
+void sol() {
+    cin >> N >> M;
+    memset(dp, -1, sizeof(dp));
+    for (int i = 1; i <= N; ++i) {
+        string s;
+        cin >> s;
+        for (int j = 1; j <= M; ++j) {
+            if (s[j - 1] == 'H') bd[i][j] = -1;
+            else bd[i][j] = s[j - 1] - '0';
+        }
+    }
+
+    cout << dfs(1, 1) << en;
+
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    while(T--) {
+        sol();
+    }
+
+    return 0;
+}
