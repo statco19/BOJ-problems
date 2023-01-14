350f305abc1c2fbde599150685e83bec2b0e60a7
andrew <statco19@gmail.com>
2023-01-14 14:57:26 +0900

    inversion counting(segtree) p4 */

--- 2517.cpp
+++ 2517.cpp
@@ -1,152 +1,78 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
 
-#define vt vector
 #define ll long long
-#define pb push_back
-#define pf push_front
-#define all(c) (c).begin(), (c).end()
-#define rev(c) (c).rbegin(), (c).rend()
-#define sz(x) (int)(x).size()
-#define mset(ar, val) memset(ar, val, sizeof(ar))
-#define vi vector<int>
-#define vvi vector<vector<int>>
-#define vll vector<long long>
-#define pii pair<int, int>
-#define pll pair<ll, ll>
-#define en '\n'
-#define ioa insert_or_assign
-#define umap unordered_map
-#define prq priority_queue
-#define F first
-#define S second
+#define vt vector
+#define pii pair<int,int>
+#define en "\n"
+#define sz(x) (x).size()
+#define all(x) (x).begin(), (x).end()
 
-#define F_OR(i, a, b, s) for (int i=(a); (s)>0?i<(b):i>(b); i+=(s))
-#define F_OR1(e) F_OR(i, 0, e, 1)
-#define F_OR2(i, e) F_OR(i, 0, e, 1)
-#define F_OR3(i, b, e) F_OR(i, b, e, 1)
-#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
-#define GET5(a, b, c, d, e, ...) e
-#define F_ORC(...) GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1)
-#define FOR(...) F_ORC(__VA_ARGS__)(__VA_ARGS__)
-
-#define E_ACH2(x, a) for (auto &x : a)
-#define E_ACH3(x, y, a) for (auto &[x, y] : a)
-#define E_ACH4(x, y, z, a) for (auto &[x, y, z] : a)
-#define E_ACHC(...) GET5(__VA_ARGS__, E_ACH4, E_ACH3, E_ACH2)
-#define EACH(...) E_ACHC(__VA_ARGS__)(__VA_ARGS__)
-
-string to_string(char c) {
-    return string(1, c);
-}
-string to_string(bool b) {
-    return b?"true":"false";
-}
-string to_string(const char* s) {
-    return string(s);
-}
-string to_string(string s) {
-    return s;
-}
-string to_string(vt<bool> v) {
-    string res;
-    FOR(sz(v))
-        res+=char('0'+v[i]);
-    return res;
-}
-
-vector<string> split (const string &s, char delim) {
-    vector<string> result;
-    stringstream ss (s);
-    string item;
-
-    while (getline (ss, item, delim)) {
-        result.push_back (item);
-    }
-
-    return result;
-}
-
-vector<string> split (const string &s, string delimiter) {
-    size_t pos_start = 0, pos_end, delim_len = delimiter.length();
-    string token;
-    vector<string> res;
-
-    while ((pos_end = s.find (delimiter, pos_start)) != string::npos) {
-        token = s.substr (pos_start, pos_end - pos_start);
-        pos_start = pos_end + delim_len;
-        res.push_back (token);
-    }
-
-    res.push_back (s.substr (pos_start));
-    return res;
-}
-
-const int d4r[4] = {-1,0,1,0}; const int d4c[4] = {0,1,0,-1};
-const int d8r[8] = {-1,-1,0,1,1,1,0,-1}; const int d8c[8] = {0,1,1,1,0,-1,-1,-1};
-const int INF = 0x3f3f3f3f; const int mINF = 0xc0c0c0c0;
-const ll LINF = 0x3f3f3f3f3f3f3f3f; const ll mLINF = 0xc0c0c0c0c0c0c0c0;
+using namespace std;
 int T = 1;
 
 const int mx = 5e5;
 int N;
-int t[2 * mx];
+vt<int> v;
+int t[2*mx];
 
 void update(int p, int val) {
-    for (t[p += N] = val; p > 1; p >>= 1) t[p >> 1] = t[p] + t[p ^ 1];
+	for(t[p+=N]=val; p>1; p>>=1) t[p>>1] = t[p] + t[p^1];
 }
 
 int qry(int l, int r) {
-    int res = 0;
-    for (l += N, r += N; l < r; l >>= 1, r >>= 1) {
-        if (l & 1) res += t[l++];
-        if (r & 1) res += t[--r];
-    }
-    return res;
+	int res = 0;
+	for(l+=N, r+=N; l<r; l>>=1, r>>=1) {
+		if(l&1) res += t[l++];
+		if(r&1) res += t[--r];
+	}
+	return res;
 }
 
 bool cmp1(pii &a, pii &b) {
-    return a.first < b.first;
+	return a.first < b.first;
 }
+
 bool cmp2(pii &a, pii &b) {
-    return a.second < b.second;
+	return a.second < b.second;
 }
 
 void sol() {
-    cin >> N;
-    vt<pii> v(N);
+	cin >> N;
+	v.resize(N);
+	for(int &x : v) cin >> x;
 
-    // coordinate compression
-    for (int i = 0; i < N; ++i) {
-        int x;
-        cin >> x;
-        v[i] = { x,i };
-    }
-    sort(v.begin(), v.end(), cmp1);
-    
-    for (int i = 0; i < N; ++i) {
-        v[i].first = i;  // relative ability
-    }
-    sort(v.begin(), v.end(), cmp2);
-    
-    // count by segment tree
-    for (int i = 0; i < N; ++i) {
-        int relativeAbility = v[i].first;
-        int idx = v[i].second;
+	vt<pii> u(N);
+	for(int i=0; i<sz(v); ++i) {
+		int ab = v[i];
+		u[i] = {v[i],i};
+	}
 
-        update(relativeAbility, 1);
-        cout << idx - qry(0, relativeAbility) + 1 << en;
-    }
+	sort(all(u), cmp1);
+	for(int i=0; i<sz(u); ++i) {
+		int idx = u[i].second;
+		u[i] = {i,idx};
+	}
 
-    return;
+	sort(all(u), cmp2);
+	for(int i=0; i<sz(u); ++i) {
+		int ab = u[i].first;
+		int idx = u[i].second;
+		cout << (idx+1) - qry(0,ab) << en;
+		update(ab,1);
+	}
+
+	return;
 }
 
 int main() {
-    ios_base::sync_with_stdio(0); cin.tie(0);
+	ios_base::sync_with_stdio(0); cin.tie(0);
 
-    while(T--) {
-        sol();
-    }
+	while(T--) {
+		sol();
+	}
 
-    return 0;
+	return 0;
 }
