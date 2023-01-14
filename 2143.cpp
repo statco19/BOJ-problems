672e00c263b33008bb530ca02d83650c92151868
andrew <statco19@gmail.com>
2023-01-14 17:09:39 +0900

    bs g3 */

--- 2143.cpp
+++ 2143.cpp
@@ -1,153 +1,70 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
 
-#define vt vector
 #define ll long long
+#define vt vector
+#define pii pair<int,int>
+#define en "\n"
 #define pb push_back
-#define pf push_front
-#define all(c) (c).begin(), (c).end()
-#define rev(c) (c).rbegin(), (c).rend()
-#define sz(x) (int)(x).size()
-#define mset(ar, val) memset(ar, val, sizeof(ar))
-#define vi vector<int>
-#define vvi vector<vector<int>>
-#define vll vector<long long>
-#define pii pair<int, int>
-#define pll pair<ll, ll>
-#define en '\n'
-#define ioa insert_or_assign
-#define umap unordered_map
-#define prq priority_queue
-#define F first
-#define S second
+#define sz(x) (x).size()
+#define all(x) (x).begin(), (x).end()
 
-#define F_OR(i, a, b, s) for (int i=(a); (s)>0?i<(b):i>(b); i+=(s))
-#define F_OR1(e) F_OR(i, 0, e, 1)
-#define F_OR2(i, e) F_OR(i, 0, e, 1)
-#define F_OR3(i, b, e) F_OR(i, b, e, 1)
-#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
-#define GET5(a, b, c, d, e, ...) e
-#define F_ORC(...) GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1)
-#define FOR(...) F_ORC(__VA_ARGS__)(__VA_ARGS__)
-
-#define E_ACH2(x, a) for (auto &x : a)
-#define E_ACH3(x, y, a) for (auto &[x, y] : a)
-#define E_ACH4(x, y, z, a) for (auto &[x, y, z] : a)
-#define E_ACHC(...) GET5(__VA_ARGS__, E_ACH4, E_ACH3, E_ACH2)
-#define EACH(...) E_ACHC(__VA_ARGS__)(__VA_ARGS__)
-
-string to_string(char c) {
-    return string(1, c);
-}
-string to_string(bool b) {
-    return b?"true":"false";
-}
-string to_string(const char* s) {
-    return string(s);
-}
-string to_string(string s) {
-    return s;
-}
-string to_string(vt<bool> v) {
-    string res;
-    FOR(sz(v))
-        res+=char('0'+v[i]);
-    return res;
-}
-
-vector<string> split (const string &s, char delim) {
-    vector<string> result;
-    stringstream ss (s);
-    string item;
-
-    while (getline (ss, item, delim)) {
-        result.push_back (item);
-    }
-
-    return result;
-}
-
-vector<string> split (const string &s, string delimiter) {
-    size_t pos_start = 0, pos_end, delim_len = delimiter.length();
-    string token;
-    vector<string> res;
-
-    while ((pos_end = s.find (delimiter, pos_start)) != string::npos) {
-        token = s.substr (pos_start, pos_end - pos_start);
-        pos_start = pos_end + delim_len;
-        res.push_back (token);
-    }
-
-    res.push_back (s.substr (pos_start));
-    return res;
-}
-
-const int d4r[4] = {-1,0,1,0}; const int d4c[4] = {0,1,0,-1};
-const int d8r[8] = {-1,-1,0,1,1,1,0,-1}; const int d8c[8] = {0,1,1,1,0,-1,-1,-1};
-const int INF = 0x3f3f3f3f; const int mINF = 0xc0c0c0c0;
-const ll LINF = 0x3f3f3f3f3f3f3f3f; const ll mLINF = 0xc0c0c0c0c0c0c0c0;
+using namespace std;
 int T = 1;
 
-int A[1001], B[1001];
-int psumA[1001], psumB[1001];
-vt<int> a, b;
 int t;
-int n, m;
+int n,m;
+vt<int> a,b;
 ll ans;
 
 void sol() {
-    cin >> t;
-    cin >> n;
-    for (int i = 1; i <= n; ++i) {
-        cin >> A[i];
-    }
+	cin >> t;
+	cin >> n;
+	a.resize(n);
+	for(int &x : a) cin >> x;
 
-    cin >> m;
-    for (int i = 1; i <= m; ++i) {
-        cin >> B[i];
-    }
+	cin >> m;
+	b.resize(m);
+	for(int &x : b) cin >> x;
 
-    for (int i = 1; i <= n; ++i) {
-        psumA[i] = A[i] + psumA[i - 1];
-        a.pb(psumA[i]);
-    }
-    for (int i = 1; i <= m; ++i) {
-        psumB[i] = B[i] + psumB[i - 1];
-        b.pb(psumB[i]);
-    }
+	vt<int> sa,sb;
+	for(int i=0; i<n; ++i) {
+		int sum = a[i];
+		sa.pb(sum);
+		for(int j=i+1; j<n; ++j) {
+			sum += a[j];
+			sa.pb(sum);
+		}
+	}
 
-    for (int i = 1; i < n; ++i) {
-        for (int j = i + 1; j <= n; ++j) {
-            a.pb(psumA[j] - psumA[i]);
-        }
-    }
+	for(int i=0; i<m; ++i) {
+		int sum = b[i];
+		sb.pb(sum);
+		for(int j=i+1; j<m; ++j) {
+			sum += b[j];
+			sb.pb(sum);
+		}
+	}
 
-    for (int i = 1; i < m; ++i) {
-        for (int j = i + 1; j <= m; ++j) {
-            b.pb(psumB[j] - psumB[i]);
-        }
-    }
-    sort(b.begin(), b.end());
-    
-    int lb, ub;
-    for (int i = 0; i < a.size(); ++i) {
-        int k = a[i];
-        int x = t - k;
-        
-        ans += upper_bound(b.begin(), b.end(), x) - lower_bound(b.begin(), b.end(), x);
-    }
+	sort(all(sa)); sort(all(sb));
+	for(int i=0; i<sz(sa); ++i) {
+		int sum = sa[i];
+		
+		ans += upper_bound(all(sb), t-sum) - lower_bound(all(sb), t-sum);
+	}
+	cout << ans << en;
 
-    cout << ans << en;
-
-    return;
+	return;
 }
 
 int main() {
-    ios_base::sync_with_stdio(0); cin.tie(0);
+	ios_base::sync_with_stdio(0); cin.tie(0);
 
-    while(T--) {
-        sol();
-    }
+	while(T--) {
+		sol();
+	}
 
-    return 0;
+	return 0;
 }
