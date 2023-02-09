ba08bc8dc7e777105dd65955f859e6956687b52d
andrew <statco19@gmail.com>
2023-02-09 22:39:18 +0900

    math, calculus s5

---
+++ 26517.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#define vt vector
+#define en '\n'
+#define ll long long
+#define ld long double
+#define pb push_back
+#define pii pair<int,int>
+#define sz(x) (x).size()
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+const int INF = 0x3f3f3f3f; const int mINF = 0xc0c0c0c0;
+const ll LINF = 0x3f3f3f3f3f3f3f3f; const ll mLINF = 0xc0c0c0c0c0c0c0c0;
+int T = 1;
+
+void sol() {
+	ll k,a,b,c,d;
+	cin >> k;
+	cin >> a >> b >> c >> d;
+
+	if(a*k + b == c*k + d) {
+		cout << "Yes " << a*k + b << en;
+	} else {
+		cout << "No" << en;
+	}
+
+	return;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	while(T--) {
+		sol();
+	}
+
+	return 0;
+}
