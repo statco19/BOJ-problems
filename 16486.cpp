b36bae24d5c422c7f645546f1a62ae26b1710497
andrew <statco19@gmail.com>
2023-02-12 15:23:54 +0900

    geometry, math b4

---
+++ 16486.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+#define vt vector
+#define en '\n'
+#define ll long long
+#define ld long double
+#define pb push_back
+#define pii pair<int,int>
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define mid (st+end)/2
+
+using namespace std;
+const int INF = 0x3f3f3f3f; const int mINF = 0xc0c0c0c0;
+const ll LINF = 0x3f3f3f3f3f3f3f3f; const ll mLINF = 0xc0c0c0c0c0c0c0c0;
+int T = 1;
+
+
+void sol() {
+	const double pi = 3.141592;
+	ll d1,d2;
+	cin >> d1 >> d2;
+
+	cout << fixed; cout.precision(9);
+	cout << 2*pi*d2 + 2*d1 << en;
+
+	return;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	while(T--) {
+		sol();
+	}
+
+	return 0;
+}
