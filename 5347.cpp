7f16b81918f2d3e373cd757d7db58df73bf07b21
andrew <statco19@gmail.com>
2023-02-18 00:13:37 +0900

    math, number theory, euclidean s5

---
+++ 5347.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+#define vt vector
+#define en '\n'
+#define ll long long
+#define ld long double
+#define pb push_back
+#define pii pair<int,int>
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define rev(c) (c).rbegin(), (c).rend()
+#define mid (st+end)/2
+
+using namespace std;
+const int INF = 0x3f3f3f3f; const int mINF = 0xc0c0c0c0;
+const ll LINF = 0x3f3f3f3f3f3f3f3f; const ll mLINF = 0xc0c0c0c0c0c0c0c0;
+int T = 1;
+
+ll gcd(ll a, ll b) { return b==0 ? a : gcd(b, a%b); }
+
+void sol() {
+	int n;
+	cin >> n;
+
+	ll a,b;
+	while(n--) {
+		cin >> a >> b;
+		cout << a*b/gcd(a,b) << en;
+	}
+
+	return;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	while(T--) {
+		sol();
+	}
+
+	return 0;
+}
