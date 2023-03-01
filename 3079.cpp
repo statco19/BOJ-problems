34a33e82295fa588da87425a9015dfb4a5ab1ba9
andrew <statco19@gmail.com>
2023-03-01 22:23:40 +0900

    bs, parametric search g5

--- 3079.cpp
+++ 3079.cpp
@@ -1,35 +1,39 @@
 #include <bits/stdc++.h>
 
-#define ll long long
 #define vt vector
 #define en '\n'
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define ioa insert_or_assign
+#define umap unordered_map
+#define prq priority_queue
 #define pb push_back
 #define pii pair<int,int>
-#define prq priority_queue
-#define sz(x) (x).size()
+#define sz(x) (int)(x).size()
 #define all(x) (x).begin(), (x).end()
-#define rev(x) (x).rbegin(), (x).rend()
+#define rev(c) (c).rbegin(), (c).rend()
+#define mset(ar, val) memset(ar, val, sizeof(ar))
 
 using namespace std;
-
 const int INF = 0x3f3f3f3f; const int mINF = 0xc0c0c0c0;
 const ll LINF = 0x3f3f3f3f3f3f3f3f; const ll mLINF = 0xc0c0c0c0c0c0c0c0;
-const int d4r[4] = { -1,0,1,0 }; const int d4c[4] = { 0,1,0,-1 };
 int T = 1;
+
 
 void sol() {
 	int N,M;
 	cin >> N >> M;
-	vt<ll> v(N);
-	for(ll &x : v) cin >> x;
+	vt<ull> v(N);
+	for(ull &x : v) cin >> x;
 		
-	ll ans = *max_element(all(v)) * M;
-	ll l=0, r=*max_element(all(v)) * M;
+	ull ans = *max_element(all(v)) * M;
+	ull l=0, r=*max_element(all(v)) * M;
 	while(l+1 < r) {
-		ll mid = (l+r) / 2;
+		ull mid = (l+r) / 2;
 		
-		ll cnt = 0;
-		for(ll x : v) {
+		ull cnt = 0;
+		for(ull x : v) {
 			cnt += mid / x;
 		}
 		
@@ -46,12 +50,13 @@ void sol() {
 	return;
 }
 
-int main() {
-	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
-	while (T--) {
+int main(){
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	while(T--) {
 		sol();
 	}
 
-	return 0;
+   return 0;
 }
