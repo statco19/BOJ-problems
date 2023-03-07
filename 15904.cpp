5525637a054e4f6fbeab17b773e080be1ad735ba
andrew <statco19@gmail.com>
2023-03-07 22:42:38 +0900

    strings, greedy s5

---
+++ 15904.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#include <quadmath.h>
+
+#define vt vector
+#define en '\n'
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define ioa insert_or_assign
+#define umap unordered_map
+#define prq priority_queue
+#define pb push_back
+#define pii pair<int,int>
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define rev(c) (c).rbegin(), (c).rend()
+#define mset(ar, val) memset(ar, val, sizeof(ar))
+
+using namespace std;
+const int d4r[4] = {-1,0,1,0}; const int d4c[4] = {0,1,0,-1};
+const int d8r[8] = {-1,-1,0,1,1,1,0,-1}; const int d8c[8] = {0,1,1,1,0,-1,-1,-1};
+const int INF = 0x3f3f3f3f; const int mINF = 0xc0c0c0c0;
+const ll LINF = 0x3f3f3f3f3f3f3f3f; const ll mLINF = 0xc0c0c0c0c0c0c0c0;
+int T = 1;
+
+void sol() {
+	string s;
+	getline(cin, s);
+	vt<char> v = {'U', 'C', 'P', 'C'};
+	int idx = 0;
+	for(int i=0; i<sz(s); ++i) {
+		if(v[idx] == s[i]) {
+			if(++idx == 4) {
+				break;
+			}
+		}
+	}
+	if(idx == 4) {
+		cout << "I love UCPC" << en;
+	} else {
+		cout << "I hate UCPC" << en;
+	}
+
+	return;
+}
+
+
+int main(){
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	while(T--) {
+		sol();
+	}
+
+   return 0;
+}
