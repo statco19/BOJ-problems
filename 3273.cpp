b36645a9af591c5ca0c9aa0c9970fe3c083a7345
andrew <statco19@gmail.com>
2023-03-14 22:48:06 +0900

    hashing, two pointers s3

---
+++ 3273.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#include <quadmath.h>
+
+#define vt vector
+#define en '\n'
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define ioa insert_or_assign
+#define umap unordered_map
+#define prq priority_queue
+#define pb push_back
+#define pii pair<int,int>
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define rev(c) (c).rbegin(), (c).rend()
+#define mset(ar, val) memset(ar, val, sizeof(ar))
+
+using namespace std;
+const int d4r[4] = {-1,0,1,0}; const int d4c[4] = {0,1,0,-1};
+const int d8r[8] = {-1,-1,0,1,1,1,0,-1}; const int d8c[8] = {0,1,1,1,0,-1,-1,-1};
+const int INF = 0x3f3f3f3f; const int mINF = 0xc0c0c0c0;
+const ll LINF = 0x3f3f3f3f3f3f3f3f; const ll mLINF = 0xc0c0c0c0c0c0c0c0;
+int T = 1;
+
+void sol() {
+	umap<int, int> u;
+	int n;
+	cin >> n;
+	vt<int> v(n);
+	for(int& x : v) cin >> x;
+
+	int x;
+	cin >> x;
+
+	int ans = 0;
+	for(int i=0; i<n; ++i) {
+		if(u.find(x-v[i]) != u.end()) {
+			ans++;
+		}
+		u[v[i]] = x-v[i];
+	}
+
+	cout << ans << en;
+
+	return;
+}
+
+
+int main(){
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	while(T--) {
+		sol();
+	}
+
+   return 0;
+}
