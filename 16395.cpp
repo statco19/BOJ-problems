93c4c622af4f736e2ed5cdd66030631444897260
andrew <statco19@gmail.com>
2023-03-21 23:06:49 +0900

    dp, math, combinatorics s5

---
+++ 16395.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#include <quadmath.h>
+
+#define vt vector
+#define en '\n'
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define ioa insert_or_assign
+#define umap unordered_map
+#define prq priority_queue
+#define pb push_back
+#define pii pair<int,int>
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define rev(c) (c).rbegin(), (c).rend()
+#define mset(ar, val) memset(ar, val, sizeof(ar))
+
+using namespace std;
+const int d4r[4] = {-1,0,1,0}; const int d4c[4] = {0,1,0,-1};
+const int d8r[8] = {-1,-1,0,1,1,1,0,-1}; const int d8c[8] = {0,1,1,1,0,-1,-1,-1};
+const int INF = 0x3f3f3f3f; const int mINF = 0xc0c0c0c0;
+const ll LINF = 0x3f3f3f3f3f3f3f3f; const ll mLINF = 0xc0c0c0c0c0c0c0c0;
+int T = 1;
+
+void sol() {
+	int dp[32][32] = {0};
+	int n,k;
+	cin >> n >> k;
+
+	for(int i=1; i<=n; ++i) {
+		dp[i][1] = dp[i][i] = 1;
+	}
+	for(int i=3; i<=n; ++i) {
+		for(int j=2; j<i; ++j) {
+			dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+		}
+	}
+
+	cout << dp[n][k] << en;
+
+	return;
+}
+
+
+int main(){
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	while(T--) {
+		sol();
+	}
+
+   return 0;
+}
