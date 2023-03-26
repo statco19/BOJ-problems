9718dc2942e86194da9267a118973a0eadb6980b
andrew <statco19@gmail.com>
2023-03-26 22:48:33 +0900

    hashing, strings s4

---
+++ 1302.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+#define vt vector
+#define en '\n'
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define ioa insert_or_assign
+#define umap unordered_map
+#define prq priority_queue
+#define pb push_back
+#define pii pair<int,int>
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define rev(c) (c).rbegin(), (c).rend()
+#define mset(ar, val) memset(ar, val, sizeof(ar))
+
+using namespace std;
+const int d4r[4] = {-1,0,1,0}; const int d4c[4] = {0,1,0,-1};
+const int d8r[8] = {-1,-1,0,1,1,1,0,-1}; const int d8c[8] = {0,1,1,1,0,-1,-1,-1};
+const int INF = 0x3f3f3f3f; const int mINF = 0xc0c0c0c0;
+const ll LINF = 0x3f3f3f3f3f3f3f3f; const ll mLINF = 0xc0c0c0c0c0c0c0c0;
+int T = 1;
+
+void sol() {
+	int n;
+	cin >> n;
+	umap<string, int> u;
+	string s;
+	for(int i=0; i<n; ++i) {
+		cin >> s;
+		u[s]++;
+	}
+
+	int mx = 0;
+	string t;
+	for(auto it=u.begin(); it!=u.end(); ++it) {
+		string a = it->first;
+		int cnt = it->second;
+
+		if(cnt > mx) {
+			mx = cnt;
+			t = a;
+		} else if(cnt == mx) {
+			if(a < t) {
+				t = a;
+			}
+		}
+	}
+
+	cout << t << en;
+
+	return;
+}
+
+
+int main(){
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	while(T--) {
+		sol();
+	}
+
+   return 0;
+}
