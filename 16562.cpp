db49cf398ca2794f80457b556c2f77e9d005ebe6
andrew <statco19@gmail.com>
2023-04-04 19:36:41 +0900

    union-find g4

---
+++ 16562.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+
+#define vt vector
+#define en '\n'
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define ioa insert_or_assign
+#define umap unordered_map
+#define prq priority_queue
+#define pb push_back
+#define pii pair<int,int>
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define rev(c) (c).rbegin(), (c).rend()
+#define mset(ar, val) memset(ar, val, sizeof(ar))
+
+using namespace std;
+const int d4r[4] = {-1,0,1,0}; const int d4c[4] = {0,1,0,-1};
+const int d8r[8] = {-1,-1,0,1,1,1,0,-1}; const int d8c[8] = {0,1,1,1,0,-1,-1,-1};
+const int INF = 0x3f3f3f3f; const int mINF = 0xc0c0c0c0;
+const ll LINF = 0x3f3f3f3f3f3f3f3f; const ll mLINF = 0xc0c0c0c0c0c0c0c0;
+int T = 1;
+
+int N,M,k;
+int parent[10001];
+int cost[10001];
+vt<int> adj[10001];
+
+int find(int x) {
+	if(parent[x] == x) {
+		return x;
+	}
+
+	return parent[x] = find(parent[x]);
+}
+
+void merge(int x, int y) {
+	x = find(x);
+	y = find(y);
+
+	if(x == y) return;
+
+	if(cost[x] < cost[y]) parent[y] = x;
+	else parent[x] = y;
+}
+
+bool isUnion(int x, int y) {
+	x = find(x);
+	y = find(y);
+
+	return x == y;
+}
+
+void sol() {
+	cin >> N >> M >> k;
+	for(int i=1,x; i<=N; ++i) {
+		parent[i] = i;
+		cin >> x;
+		cost[i] = x;
+	}
+	for(int i=1,u,v; i<=M; ++i) {
+		cin >> u >> v;
+		merge(u,v);
+	}
+
+	int ans = 0;
+	for(int i=1; i<=N; ++i) {
+		if(find(i) == i) {
+			ans += cost[i];
+		}
+	}
+
+	if(ans <= k) cout << ans << en;
+	else cout << "Oh no" << en;
+
+	return;
+}
+
+
+int main(){
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	while(T--) {
+		sol();
+	}
+
+   return 0;
+}
