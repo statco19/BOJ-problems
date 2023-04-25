d68834e40768d379ca57d632fb06e20dc9b2b2d5
andrew <statco19@gmail.com>
2023-04-25 22:03:46 +0900

    strings, parsing b1

---
+++ 12605.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+#define vt vector
+#define en '\n'
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define ioa insert_or_assign
+#define umap unordered_map
+#define prq priority_queue
+#define pb push_back
+#define pii pair<int,int>
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define rev(c) (c).rbegin(), (c).rend()
+#define mset(ar, val) memset(ar, val, sizeof(ar))
+
+using namespace std;
+const int d4r[4] = {-1,0,1,0}; const int d4c[4] = {0,1,0,-1};
+const int d8r[8] = {-1,-1,0,1,1,1,0,-1}; const int d8c[8] = {0,1,1,1,0,-1,-1,-1};
+const int INF = 0x3f3f3f3f; const int mINF = 0xc0c0c0c0;
+const ll LINF = 0x3f3f3f3f3f3f3f3f; const ll mLINF = 0xc0c0c0c0c0c0c0c0;
+int T = 1;
+
+vector<string> split (const string &s, char delim) {
+    vector<string> result;
+    stringstream ss (s);
+    string item;
+
+    while (getline (ss, item, delim)) {
+        result.push_back (item);
+    }
+
+    return result;
+}
+
+void sol() {
+	int n;
+	cin >> n;
+	string s;
+	getline(cin, s);
+	for(int i=1; i<=n; ++i) {
+		getline(cin, s);
+		vt<string> v = split(s, ' ');
+		cout << "Case #" << i << ": ";
+		for(int j=sz(v)-1; ~j; --j) {
+			cout << v[j];
+			if(j != 0) cout << ' ';
+			else cout << en;
+		}
+	}
+
+	return;
+}
+
+
+int main(){
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	while(T--) {
+		sol();
+	}
+
+   return 0;
+}
