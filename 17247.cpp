85706ea2bf40a1643e4999112593ac43fc45c1a7
andrew <statco19@gmail.com>
2023-04-28 19:04:03 +0900

    geometry, math b1

---
+++ 17247.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+#define vt vector
+#define en '\n'
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define ioa insert_or_assign
+#define umap unordered_map
+#define prq priority_queue
+#define pb push_back
+#define pii pair<int,int>
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define rev(c) (c).rbegin(), (c).rend()
+#define mset(ar, val) memset(ar, val, sizeof(ar))
+
+using namespace std;
+const int d4r[4] = {-1,0,1,0}; const int d4c[4] = {0,1,0,-1};
+const int d8r[8] = {-1,-1,0,1,1,1,0,-1}; const int d8c[8] = {0,1,1,1,0,-1,-1,-1};
+const int INF = 0x3f3f3f3f; const int mINF = 0xc0c0c0c0;
+const ll LINF = 0x3f3f3f3f3f3f3f3f; const ll mLINF = 0xc0c0c0c0c0c0c0c0;
+int T = 1;
+
+void sol() {
+	int n,m;
+	cin >> n >> m;
+	pii p1, p2;
+	int cnt = 0;
+	int x;
+	for(int i=1; i<=n; ++i) {
+		for(int j=1; j<=m; ++j) {
+			cin >> x;
+			if(x) {
+				if(!cnt) {
+					p1 = {i,j};
+					cnt++;
+				} else {
+					p2 = {i,j};
+				}
+			}
+		}
+	}
+
+	cout << abs(p1.first-p2.first) + abs(p1.second-p2.second) << en;
+
+	return;
+}
+
+
+int main(){
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	while(T--) {
+		sol();
+	}
+
+   return 0;
+}
