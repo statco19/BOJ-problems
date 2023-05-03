df70bac1cd45fc4753c152b55ffe0eabc44123b1
andrew <statco19@gmail.com>
2023-05-03 23:02:56 +0900

    impl, math b1

---
+++ 1924.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+#define vt vector
+#define en '\n'
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define ioa insert_or_assign
+#define umap unordered_map
+#define prq priority_queue
+#define pb push_back
+#define pii pair<int,int>
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define rev(c) (c).rbegin(), (c).rend()
+#define mset(ar, val) memset(ar, val, sizeof(ar))
+
+using namespace std;
+const int d4r[4] = {-1,0,1,0}; const int d4c[4] = {0,1,0,-1};
+const int d8r[8] = {-1,-1,0,1,1,1,0,-1}; const int d8c[8] = {0,1,1,1,0,-1,-1,-1};
+const int INF = 0x3f3f3f3f; const int mINF = 0xc0c0c0c0;
+const ll LINF = 0x3f3f3f3f3f3f3f3f; const ll mLINF = 0xc0c0c0c0c0c0c0c0;
+int T = 1;
+
+void sol() {
+	vt<int> v = {31,28,31,30,31,30,31,31,30,31,30,31};
+	vt<string> u = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+	int x,y;
+	cin >> x >> y;
+	int day = 0;
+	if(x > 1) {
+		for(int i=0; i<x-1; ++i) {
+			day += v[i];
+		}
+	}
+	day += y;
+	day %= 7;
+
+	cout << u[day] << en;
+
+	return;
+}
+
+
+int main(){
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	
+	while(T--) {
+		sol();
+	}
+
+   return 0;
+}
