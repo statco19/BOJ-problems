ed41be19a66b6ca8aed0c0a228ad9b2e31e62c92
andrew <statco19@gmail.com>
2023-07-09 22:35:03 +0900

    impl, sorting s5

---
+++ 5800.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+#define vt vector
+#define en '\n'
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define ioa insert_or_assign
+#define umap unordered_map
+#define prq priority_queue
+#define pb push_back
+#define pii pair<int,int>
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define rev(c) (c).rbegin(), (c).rend()
+#define mset(ar, val) memset(ar, val, sizeof(ar))
+
+using namespace std;
+const int d4r[4] = {-1,0,1,0}; const int d4c[4] = {0,1,0,-1};
+const int d8r[8] = {-1,-1,0,1,1,1,0,-1}; const int d8c[8] = {0,1,1,1,0,-1,-1,-1};
+const int INF = 0x3f3f3f3f; const int mINF = 0xc0c0c0c0;
+const ll LINF = 0x3f3f3f3f3f3f3f3f; const ll mLINF = 0xc0c0c0c0c0c0c0c0;
+int T = 1;
+
+void sol() {
+	int n; cin >> n;
+	int x;
+	for(int t=1; t<=n; ++t) {
+		cin >> x;
+		vt<int> v(x);
+		for(int &a : v) cin >> a;
+		sort(rev(v));
+		cout << "Class " << t << en;
+		cout << "Max " << v.front() << ", Min " << v.back() << ", Largest gap ";
+
+		int gap = mINF;
+		for(int i=0; i<sz(v)-1; ++i) {
+			gap = max(gap, v[i]-v[i+1]);
+		}
+		cout << gap << en;
+	}	
+
+	return;
+}
+
+
+int main(){
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	while(T--) {
+		sol();
+	}
+
+   return 0;
+}
