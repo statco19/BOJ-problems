c70d29661a54cf3436d1d64d741c92809daa3f13
andrew <statco19@gmail.com>
2023-07-15 22:15:59 +0900

    sorting, two pointers s4

---
+++ 1940.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+#define vt vector
+#define en '\n'
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define ioa insert_or_assign
+#define umap unordered_map
+#define prq priority_queue
+#define pb push_back
+#define pii pair<int,int>
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define rev(c) (c).rbegin(), (c).rend()
+#define mset(ar, val) memset(ar, val, sizeof(ar))
+
+using namespace std;
+const int d4r[4] = {-1,0,1,0}; const int d4c[4] = {0,1,0,-1};
+const int d8r[8] = {-1,-1,0,1,1,1,0,-1}; const int d8c[8] = {0,1,1,1,0,-1,-1,-1};
+const int INF = 0x3f3f3f3f; const int mINF = 0xc0c0c0c0;
+const ll LINF = 0x3f3f3f3f3f3f3f3f; const ll mLINF = 0xc0c0c0c0c0c0c0c0;
+int T = 1;
+
+void sol() {
+	int n; cin >> n;
+	int m; cin >> m;
+	vt<int> v(n);
+	for(int& x : v) cin >> x;
+	sort(all(v));
+
+	int l=0,r=n-1;
+	int ans = 0;
+	int sum = v[l] + v[r];
+	while(l<r) {
+		if(sum > m) {
+			sum -= v[r];
+			sum += v[--r];
+		}
+		else {
+			if(sum == m) ans++;
+			sum -= v[l];
+			sum += v[++l];
+		}
+	}
+	cout << ans << en;
+
+	return;
+}
+
+
+int main(){
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	while(T--) {
+		sol();
+	}
+
+   return 0;
+}
