a4ad787b122f28def7e3d4499137a998ca91f908
andrew <statco19@gmail.com>
2023-07-27 00:04:54 +0900

    arithmetic, math b4

---
+++ 2530.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+#define vt vector
+#define en '\n'
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define ioa insert_or_assign
+#define umap unordered_map
+#define prq priority_queue
+#define pb push_back
+#define pii pair<int,int>
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define rev(c) (c).rbegin(), (c).rend()
+#define mset(ar, val) memset(ar, val, sizeof(ar))
+
+using namespace std;
+const int d4r[4] = {-1,0,1,0}; const int d4c[4] = {0,1,0,-1};
+const int d8r[8] = {-1,-1,0,1,1,1,0,-1}; const int d8c[8] = {0,1,1,1,0,-1,-1,-1};
+const int INF = 0x3f3f3f3f; const int mINF = 0xc0c0c0c0;
+const ll LINF = 0x3f3f3f3f3f3f3f3f; const ll mLINF = 0xc0c0c0c0c0c0c0c0;
+int T = 1;
+
+void sol() {
+	int t = 0;
+	int A,B,C,D;
+	cin >> A >> B >> C >> D;
+	t += A * 3600 + B * 60 + C + D;
+	A = t/3600;
+	t -= A * 3600;
+	if(A >= 24) A %= 24;
+	B = t/60;
+	t -= B * 60;
+	C = t % 60;
+	cout << A << ' ' << B << ' ' << C << en;
+
+	return;
+}
+
+
+int main(){
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	while(T--) {
+		sol();
+	}
+
+   return 0;
+}
