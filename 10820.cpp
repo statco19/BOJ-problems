d53aa4f85592d00ae9b7718e1c98981316ba85e3
andrew <statco19@gmail.com>
2023-08-01 21:56:10 +0900

    impl, string b2

---
+++ 10820.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+#define vt vector
+#define en '\n'
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define ioa insert_or_assign
+#define umap unordered_map
+#define prq priority_queue
+#define pb push_back
+#define pii pair<int,int>
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define rev(c) (c).rbegin(), (c).rend()
+#define mset(ar, val) memset(ar, val, sizeof(ar))
+
+using namespace std;
+const int d4r[4] = {-1,0,1,0}; const int d4c[4] = {0,1,0,-1};
+const int d8r[8] = {-1,-1,0,1,1,1,0,-1}; const int d8c[8] = {0,1,1,1,0,-1,-1,-1};
+const int INF = 0x3f3f3f3f; const int mINF = 0xc0c0c0c0;
+const ll LINF = 0x3f3f3f3f3f3f3f3f; const ll mLINF = 0xc0c0c0c0c0c0c0c0;
+int T = 1;
+
+void sol() {
+	string s;
+	while(1) {
+		getline(cin, s);
+		if(cin.eof()) break;
+
+		int a=0,b=0,C=0,d=0;
+		for(int i=0; i<sz(s); ++i) {
+			char c = s[i];
+			if(97<=c&&c<=122) a++;
+			else if(65<=c&&c<=90) b++;
+			else if('0'<=c&&c<='9') C++;
+			else d++;
+		}
+		cout << a << ' ' << b << ' ' << C << ' ' << d << en;
+	}	
+
+	return;
+}
+
+
+int main(){
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	while(T--) {
+		sol();
+	}
+
+   return 0;
+}
